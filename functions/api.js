72a05e32cedd286e5b506906f4b2ee108f800573
thelight1231 <bondokmolto@gmail.com>
2025-01-30 12:41:24 +0200

    Fix CORS configuration for frontend domain

--- functions/api.js
+++ functions/api.js
@@ -4,7 +4,12 @@ const cors = require('cors');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: 'https://infinite112.netlify.app',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept']
+}));
 app.use(express.json());
 
 // Test route
